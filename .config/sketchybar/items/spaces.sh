ce92893435c7b32788c886890d612037e7df360f
rossebo <dev@rossebo.org>
2025-08-04 11:43:51 +0200

    Update sketchybar

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -6,7 +6,8 @@ for sid in "${SPACE_SIDS[@]}"
 do
 	sketchybar --add space space.$sid left \
 		--set space.$sid space=$sid \
-		icon=$sid
+		icon=$sid \
+		label="test" \
 		label.font="sketchybar-app-font:Regular:16.0" \
 		label.padding_right=20 \
 		label.y_offset=1 \
