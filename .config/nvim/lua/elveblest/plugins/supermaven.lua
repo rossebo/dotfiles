eb95c7eb5907b7172c8ff309302f8b08f049f8db
Petter Rossebø <petter.rossebo@spv.no>
2025-12-11 08:30:56 +0100

    Update to aerospace and wezterm

--- .config/nvim/lua/elveblest/plugins/supermaven.lua
+++ .config/nvim/lua/elveblest/plugins/supermaven.lua
@@ -1,23 +1,24 @@
-return {
-  'supermaven-inc/supermaven-nvim',
-  config = function()
-    require('supermaven-nvim').setup {
-      keymaps = {
-        accept_suggestion = '<Tab>',
-        clear_suggestion = '<C-]>',
-        accept_word = '<C-j>',
-      },
-      color = {
-        suggestion_color = '#f6f6f6',
-        cterm = 244,
-      },
-      log_level = 'info', -- set to "off" to disable logging completely
-      ignored_filetypes = { 'gitcommit', 'gitrebase', 'gitconfig', 'gitignore', 'gitmodules', 'markdown', 'text' },
-      disable_inline_completion = false, -- disables inline completion for use with cmp
-      disable_keymaps = false, -- disables built in keymaps for more manual control
-      condition = function()
-        return false
-      end,
-    }
-  end,
-}
+return {}
+-- return {
+--   'supermaven-inc/supermaven-nvim',
+--   config = function()
+--     require('supermaven-nvim').setup {
+--       keymaps = {
+--         accept_suggestion = '<Tab>',
+--         clear_suggestion = '<C-]>',
+--         accept_word = '<C-j>',
+--       },
+--       color = {
+--         suggestion_color = '#f6f6f6',
+--         cterm = 244,
+--       },
+--       log_level = 'info', -- set to "off" to disable logging completely
+--       ignored_filetypes = { 'gitcommit', 'gitrebase', 'gitconfig', 'gitignore', 'gitmodules', 'markdown', 'text' },
+--       disable_inline_completion = false, -- disables inline completion for use with cmp
+--       disable_keymaps = false, -- disables built in keymaps for more manual control
+--       condition = function()
+--         return false
+--       end,
+--     }
+--   end,
+-- }
--- .config/sketchybar/plugins/space_windows.sh
+++ .config/sketchybar/plugins/space_windows.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-#
-if [ "$SENDER" = "space_windows_change" ]; then
-  space="$(echo "$INFO" | jq -r '.space')"
-  apps="$(echo "$INFO | jq -r '.apps | keys[]')"
 
-  icon_strip=" "
-  if [ "${apps}" != "" ]; then
-    while read -r app
-    do
-      icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
-    done <<< "${apps}"
-  else
-    icon_strip=" -"
-  fi
-
-  sketchybar --set space.$space label="$icon_strip"
+if [ "$SENDER" = "aerospace_workspace_change" ]; then
+  # Get all visible workspaces
+  visible_workspaces=$(aerospace list-workspaces --format "%{id} %{workspace-is-visible}" | grep "true" | awk '{print $1}')
+  
+  # Update only visible workspaces
+  for workspace in $visible_workspaces; do
+    is_focused=$(aerospace list-workspaces --format "%{id} %{workspace-is-focused}" | grep "^$workspace " | awk '{print $2}')
+    apps=$(aerospace list-windows --workspace "$workspace" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+    
+    sketchybar --set space.$workspace drawing=on
+    
+    if [ "${apps}" != "" ]; then
+      icon_strip=" "
+      while read -r app; do
+        icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+      done <<<"${apps}"
+      sketchybar --set space.$workspace label="$icon_strip"
+    else
+      sketchybar --set space.$workspace label=""
+    fi
+  done
 fi
