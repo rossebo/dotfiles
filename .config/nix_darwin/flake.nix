76d381ecbe7fdf35856c2b04f561740c54d94119
Petter Rossebø <petter.rossebo@webstep.no>
2026-02-23 23:45:59 +0100

    nix update

--- .config/nix_darwin/flake.nix
+++ .config/nix_darwin/flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
@@ -15,6 +16,143 @@
       environment.systemPackages =
         [ pkgs.vim
         ];
+
+      system.primaryUser = "elveblest";
+
+      homebrew = {
+        enable = true;
+        onActivation = {
+          autoUpdate = true;
+          cleanup = "zap"; 
+        };
+        brews = [
+          "aom"
+          "azure-cli"
+          "azure-functions-core-tools@4"
+          "bat"
+          "borders"
+          "brotli"
+          "btop"
+          "ca-certificates"
+          "cmatrix"
+          "curl"
+          "docker"
+          "docker-completion"
+          "docker-compose"
+          "eza"
+          "fd"
+          "fontconfig"
+          "freetype"
+          "fzf"
+          "gettext"
+          "giflib"
+          "git-delta"
+          "glib"
+          "go"
+          "golangci-lint"
+          "highway"
+          "imagemagick"
+          "imath"
+          "jasper"
+          "jpeg-turbo"
+          "jpeg-xl"
+          "jq"
+          "lazygit"
+          "libde265"
+          "libdeflate"
+          "libevent"
+          "libgit2"
+          "libheif"
+          "libidn2"
+          "liblqr"
+          "libnghttp2"
+          "libnghttp3"
+          "libngtcp2"
+          "libomp"
+          "libpng"
+          "libraw"
+          "libsodium"
+          "libssh2"
+          "libtiff"
+          "libtool"
+          "libunistring"
+          "libuv"
+          "libvmaf"
+          "libyaml"
+          "libzip"
+          "little-cms2"
+          "lpeg"
+          "lua"
+          "luajit"
+          "luarocks"
+          "luv"
+          "lz4"
+          "m4"
+          "mpdecimal"
+          "ncurses"
+          "neovim"
+          "nvm"
+          "oniguruma"
+          "openexr"
+          "openjpeg"
+          "openjph"
+          "openssl@3"
+          "pcre2"
+          "pnpm"
+          "podman"
+          "python@3.13"
+          "readline"
+          "ripgrep"
+          "rtmpdump"
+          "shared-mime-info"
+          "sketchybar"
+          "skhd"
+          "sqlite"
+          "starship"
+          "stow"
+          "terraform"
+          "tlrc"
+          "tmux"
+          "tree-sitter"
+          "unibilium"
+          "utf8proc"
+          "webp"
+          "wget"
+          "x265"
+          "xz"
+          "yabai"
+          "yarn"
+          "yazi"
+          "zoxide"
+          "zstd"
+        ];
+        casks = [
+          "alfred"
+          "bazecor"
+          "blender"
+          "discord"
+          "dotnet-sdk"
+          "firefox"
+          "font-cascadia-code"
+          "font-hack-nerd-font"
+          "font-sf-pro"
+          "kitty"
+          "microsoft-auto-update"
+          "microsoft-teams"
+          "obsidian"
+          "orbstack"
+          "powershell"
+          "qutebrowser"
+          "rider"
+          "sf-symbols"
+          "slack"
+          "steam"
+          "visual-studio-code"
+          "xnviewmp"
+        ];
+        masApps = {
+        };
+      };
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
@@ -35,9 +173,27 @@
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Petters-MacBook-Pro
+    # $ darwin-rebuild build --flake .#m3
     darwinConfigurations."m3" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew 
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "elveblest";
+
+            # Automatically migrate existing Homebrew installations
+            autoMigrate = true;
+          };
+        }
+      ];
     };
   };
 }
